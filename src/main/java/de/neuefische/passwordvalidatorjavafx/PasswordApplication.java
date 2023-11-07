4f5a5dca1b3ef2bf38500cfd803520398ba0f946
Marcell <marcell.dechant@neuefische.de>
2023-11-07 12:05:23 +0100

    Updated application title to 'Password Validator'

--- src/main/java/de/neuefische/passwordvalidatorjavafx/PasswordApplication.java
+++ src/main/java/de/neuefische/passwordvalidatorjavafx/PasswordApplication.java
@@ -12,7 +12,7 @@ public class PasswordApplication extends Application {
 	public void start(Stage stage) throws IOException {
 		FXMLLoader fxmlLoader = new FXMLLoader(PasswordApplication.class.getResource("password-view.fxml"));
 		Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-		stage.setTitle("Hello!");
+		stage.setTitle("Password Validator");
 		stage.setScene(scene);
 		stage.show();
 	}
